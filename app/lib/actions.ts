d7d63aa027212bb5ccf8c3211f5ea63b3b6b0890
lee <jinglepp@qq.com>
2024-05-13 23:38:28 +0800

    fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -43,15 +43,15 @@ export async function updateInvoice(id: string, formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-
+ 
   const amountInCents = amount * 100;
-
+ 
   await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
-
+ 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
